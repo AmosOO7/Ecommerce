53a2beee6f6e85686ebf81a2ec5ebfe626b70966
AmosOO7 <ayokunene@gmail.com>
2024-10-14 00:48:20 +0100

    second commit

--- src/components/Pages/About.tsx
+++ src/components/Pages/About.tsx
@@ -2,7 +2,43 @@ import React from "react";
 import "./About.css";
 import Footer from "../AdsAndFooter/Footer";
 const About = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="about-inner">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+        suscipit amet impedit aperiam accusantium a magnam, reiciendis voluptas
+        enim odio quaerat maiores omnis ea, nesciunt debitis possimus ipsa
+        consectetur? Reprehenderit. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Aliquid suscipit amet impedit aperiam accusantium a
+        magnam, reiciendis voluptas enim odio quaerat maiores omnis ea, nesciunt
+        debitis possimus ipsa consectetur? Reprehenderit. <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+        suscipit amet impedit aperiam accusantium a magnam, reiciendis voluptas
+        enim odio quaerat maiores omnis ea, nesciunt debitis possimus ipsa
+        consectetur? Reprehenderit. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Aliquid suscipit amet impedit aperiam accusantium a
+        magnam, reiciendis voluptas enim odio quaerat maiores omnis ea, nesciunt
+        debitis possimus ipsa consectetur? Reprehenderit. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Aliquid suscipit amet impedit aperiam
+        accusantium a magnam, reiciendis voluptas enim odio quaerat maiores
+        omnis ea, nesciunt debitis possimus ipsa consectetur? Reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        <br />
+        <br />
+        Aliquid suscipit amet impedit aperiam accusantium a magnam, reiciendis
+        voluptas enim odio quaerat maiores omnis ea, nesciunt debitis possimus
+        ipsa consectetur? Reprehenderit. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Aliquid suscipit amet impedit aperiam accusantium a
+        magnam, reiciendis voluptas enim odio quaerat maiores omnis ea, nesciunt
+        debitis possimus ipsa consectetur? Reprehenderit. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Aliquid suscipit amet impedit aperiam
+        accusantium a magnam, reiciendis voluptas enim odio quaerat maiores
+        omnis ea, nesciunt debitis possimus ipsa consectetur? Reprehenderit.
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default About;
